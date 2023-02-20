84ee3613730e64a63bb6ee25a7955d72e0553ba4
shital gawade <shitalgawade131@gmail.com>
2023-02-20 09:54:41 +0530

    Fibonacci even nth

---
+++ EvenFibonacci.java
@@ -0,0 +1,34 @@
+package com;
+
+import java.util.Scanner;
+
+public class EvenFibonacci
+{
+    public static void main(String[] args)
+    {
+        int n,num1=0,num2=1,num3;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter number:-");
+        n = s.nextInt();
+
+
+        for(int i=0;i<n;i++)
+        {
+            num3=0;
+            num3=num1+num2;
+
+            if(num3%2==0)
+            {
+                System.out.println(num3);
+            }
+            num1=num2;
+            num2=num3;
+
+
+        }
+
+
+
+
+    }
+}
